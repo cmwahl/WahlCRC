68526a7c23c7739b51adf7a64a23b593289a33c7
Christopher Wahl <cmwahl@hotmail.com>
2021-09-08 00:50:09 -0400

    One last main fix to make it a better example of the different CRCs

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@ using namespace std;
 int main()
 {
 	
-	unsigned char* message = new unsigned char[6];
+	unsigned char* message = new unsigned char[10];
 
-	memset(message, 0, 6);
+	memset(message, 0, 10);
 	message[0] = 'd';
-	message[1] = '\0';
-	message[2] = '\0';
-	message[3] = '\0';
-	message[4] = '\0';
+	message[1] = 'o';
+	message[2] = 'g';
+	message[3] = 's';
+	message[4] = '!';
 
 	string messagestr = "dog  ";
 
 	WahlCRC::MessageCRC crcer(WahlCRC::CRCs[2]);
-	crcer.findRemainder((char*)message, 5);
+	crcer.findRemainder((char*)message, 9);
 
-	for (int i = 0; i < 4; ++i) {
-		message[1 + i] = crcer.getRemainder()[i];
+	for (int i = 0; i < crcer.getCRC().bits/8; ++i) {
+		message[9 - crcer.getRemainderN() + i] = crcer.getRemainder()[i];
 	}
 
 	cout << "New characters: " << endl;
@@ -32,7 +32,7 @@ int main()
 		cout << check << endl;
 	}
 
-	crcer.findRemainder((char*)message, 5);
+	crcer.findRemainder((char*)message, 9);
 
 	cout << "Check remainder: " << endl;
 	for (int i = 0; i < crcer.getRemainderN(); ++i) {
